06abad3319d121d735fa644c84b7ce952d4656a9
Wiley Lau <henloboii@protonmail.ch>
2021-09-08 16:09:13 +0800

    kernel: import houston and control_center from OnePlus 8
    * kang clk_get_ddr_freq from sm8150 as it's made for 4.14
    * backport some more stuff when we are at it
    * import im and ratp includes as we'll need them for compiling
    * drop bq fuelgauge and houston battery stuff as we don't even use it
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- drivers/power/supply/power_supply_core.c
+++ drivers/power/supply/power_supply_core.c
@@ -22,7 +22,9 @@
 #include <linux/power_supply.h>
 #include <linux/thermal.h>
 #include "power_supply.h"
-
+#ifdef CONFIG_HOUSTON
+#include <oneplus/houston/houston_helper.h>
+#endif
 /* exported for the APM Power driver, APM emulation */
 struct class *power_supply_class;
 EXPORT_SYMBOL_GPL(power_supply_class);
@@ -936,6 +938,9 @@ __power_supply_register(struct device *parent,
 	queue_delayed_work(system_power_efficient_wq,
 			   &psy->deferred_register_work,
 			   POWER_SUPPLY_DEFERRED_REGISTER_TIME);
+#ifdef CONFIG_HOUSTON
+	ht_register_power_supply(psy);
+#endif
 
 	return psy;
 
